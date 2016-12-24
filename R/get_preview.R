d65d5bce5671edd21b29bacbf7d759adde85746e
Nelson Areal <nareal@gmail.com>
2016-12-24 12:29:11 +0000

    Use xml2 instead of rvest

--- R/get_preview.R
+++ R/get_preview.R
@@ -1,9 +1,8 @@
 #!/usr/bin/Rscript
 
-library(rvest)
+library(xml2)
 page <- read_html("https://www.rstudio.com/products/rstudio/download/preview/")
-file_version <- html_text(html_nodes(page, "h2")[1])
+file_version <- xml_text(xml_find_all(page, "//h2")[1])
 file_version <- sub(".*v([0-9]\\.[0-9]*\\.[0-9]*).*", "\\1", file_version)
 file_name <- paste0("https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-", file_version, "-amd64.deb")
 download.file(file_name, method="wget", dest="rstudio-server-daily-amd64.deb")
-
