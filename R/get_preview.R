2076ae7b3c794d60ac83c69e2d2bf16648c7c47e
Nelson Areal <nareal@gmail.com>
2022-06-02 16:49:26 +0100

    Select the bionic build of Rstudio now that jammy is available

--- R/get_preview.R
+++ R/get_preview.R
@@ -3,4 +3,5 @@
 library(xml2)
 page <- read_html("https://www.rstudio.com/products/rstudio/download/preview/")
 file_name <- xml_text(xml_find_all(page, "//a[starts-with(text(), 'RStudio Server') and contains(text(), 'Ubuntu')]/@href"))
+file_name <- grep("bionic", file_name, value = TRUE)
 download.file(file_name, method="wget", dest="rstudio-server-preview-amd64.deb")
