8a46435188f3bc7fabe24e068225f8cb437c8ad3
Nelson Areal <nareal@gmail.com>
2022-11-07 09:41:25 +0000

    Fixed the download link (again)

--- R/get_preview.R
+++ R/get_preview.R
@@ -2,9 +2,16 @@
 
 library(xml2)
 page <- read_html("https://dailies.rstudio.com")
-file_name <- xml_text(xml_find_all(page, "//a[contains(@aria-label, 'RStudio Server for Ubuntu 18/20 (x86_64)')]/@href"))
-file_name <- paste0(
+builds_page_url <- xml_text(xml_find_all(page, "//a[contains(@aria-label, 'RStudio Server for Ubuntu 18/20 (x86_64)')]/@href"))
+builds_page_url <- paste0(
   "https://dailies.rstudio.com",
-  file_name
+  builds_page_url
 )
-download.file(file_name, method = "wget", dest = "rstudio-server-preview-amd64.deb")
+
+builds_page <- read_html(builds_page_url)
+file_name <- xml_text(xml_find_all(builds_page, "//a[starts-with(text(), 'rstudio-server')]/@href"))
+file_name <- file_name[1]
+download.file(file_name,
+  method = "wget",
+  dest = "rstudio-server-preview-amd64.deb"
+)
